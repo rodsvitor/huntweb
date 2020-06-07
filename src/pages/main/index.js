f7c261dec9c51bacdc8953e95e35bea5f6455e48
Rodrigo Vitor <rodrigovitor@maxtool.com.br>
2020-06-07 20:28:00 -0300

    Listando produtos

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import api from '../../services/api';
+import './styles.css';
 
 export default class Main extends Component {
   state = {//o Metodo render fica monitorando as alterações no objeto state
@@ -16,10 +17,16 @@ export default class Main extends Component {
   }
 
   render() {
+    const { products } = this.state;
+
     return <div className="product-list">
       {
-        this.state.products.map(product =>
-        <h2 key={product._id}>{product.title}</h2>)
+        products.map(product =>
+          <article key={product._id}>
+            <strong>{product.title}</strong>
+            <p>{product.description}</p>
+            <a href={""}>Acessar</a>
+          </article>)
       }
     </div>
   }
