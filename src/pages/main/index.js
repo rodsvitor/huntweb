b26fff7058d346343818a90c6e7abf2513e1f281
Rodrigo Vitor <rodrigovitor@maxtool.com.br>
2020-06-07 17:55:37 -0300

    Armazanando no estado para que o render monitore

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,16 +2,25 @@ import React, { Component } from "react";
 import api from '../../services/api';
 
 export default class Main extends Component {
+  state = {//o Metodo render fica monitorando as alterações no objeto state
+    products: [],
+  }
+
   componentDidMount() { //executa uma ação sempre que um componente é exibido em tela
     this.loadProducts();
   }
 
   loadProducts = async () => {
     const response = await api.get('/products');
-    console.log(response.data.docs);
+    this.setState({ products: response.data.docs});
   }
 
   render() {
-    return <h1>Hello Rocketseat 2</h1>
+    return <div className="product-list">
+      {
+        this.state.products.map(product =>
+        <h2 key={product._id}>{product.title}</h2>)
+      }
+    </div>
   }
 }
