297ed95a0c5335132577d09fc383ece5a50fe5a5
Rodrigo Vitor <rodrigovitor@maxtool.com.br>
2020-06-07 21:23:03 -0300

    Implementação de página anterior/próxima

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -4,30 +4,59 @@ import './styles.css';
 
 export default class Main extends Component {
   state = {//o Metodo render fica monitorando as alterações no objeto state
-    products: [],
+    docs: [],
+    productInfo: {},
+    page: 1,
   }
 
   componentDidMount() { //executa uma ação sempre que um componente é exibido em tela
     this.loadProducts();
   }
 
-  loadProducts = async () => {
-    const response = await api.get('/products');
-    this.setState({ products: response.data.docs});
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
+
+    const {docs, ...productInfo } = response.data;
+
+    this.setState({ docs, productInfo, page});
+  }
+
+  prevPage = () => {
+    const {page, productInfo } = this.state;
+
+    if (page === 1) return;
+
+    const pageNumber = page - 1;
+
+    this.loadProducts(pageNumber);
+
+  }
+
+  nextPage = () => {
+    const { page, productInfo } = this.state;
+
+    if (page === productInfo.pages) return;
+
+    const pageNumber = page + 1;
+    this.loadProducts(pageNumber);
   }
 
   render() {
-    const { products } = this.state;
+    const { docs, page, productInfo } = this.state;
 
     return <div className="product-list">
       {
-        products.map(product =>
+        docs.map(product =>
           <article key={product._id}>
             <strong>{product.title}</strong>
             <p>{product.description}</p>
             <a href={""}>Acessar</a>
           </article>)
       }
+      <div className={"actions"}>
+        <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+        <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próxima</button>
+      </div>
     </div>
   }
 }
